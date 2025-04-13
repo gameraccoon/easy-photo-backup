cef60753f244bc3a40b9a37ff5289ae94b6c32b9
Pavel Grebnev <ruwhynot@gmail.com>
2025-04-13 09:42:32 +0200

    Introduced some shell for classes for service discovery

--- client_ffi/client_ffi/src/lib.rs
+++ client_ffi/client_ffi/src/lib.rs
@@ -1,9 +1,29 @@
+use std::sync::Arc;
+use std::sync::Mutex;
+
 // generate uniffi boilerplate
 uniffi::setup_scaffolding!();
 
-#[derive(PartialEq, Debug, uniffi::Object)]
+#[derive(Debug, Clone, uniffi::Record)]
+pub struct Service {
+    pub name: String,
+    pub ip: String,
+    pub port: i16,
+}
+
+impl Service {
+    pub fn new() -> Self {
+        Self {
+            name: String::new(),
+            ip: String::new(),
+            port: 0,
+        }
+    }
+}
+
+#[derive(Debug, Clone, uniffi::Object)]
 pub struct Calculator {
-    
+    online_services: Arc<Mutex<Vec<Service>>>
 }
 
 #[uniffi::export]
@@ -11,16 +31,31 @@ impl Calculator {
     #[uniffi::constructor]
     pub fn new() -> Self {
         Self {
+            online_services: Arc::new(Mutex::new(Vec::new()))
         }
     }
 
-    /// Performs a calculation using the supplied binary operator and operands.
-    pub fn calculate(
-        &self,
-        lhs: i64,
-        rhs: i64,
-    ) -> i64 {
-        lhs + rhs
+    pub fn start(&self) {
+        self.online_services.lock().unwrap().push(Service{
+            name: "Test".to_string(),
+            ip: "0.0.0.0".to_string(),
+            port: 42,
+        });
+    }
+
+    pub fn stop(&self, wait_for_thread_join: bool) {
+
+    }
+
+    pub fn get_services(&self) -> Vec<Service> {
+        let services = self.online_services.lock();
+        if let Ok(services) = services {
+            services.clone()
+        }
+        else
+        {
+            vec![]
+        }
     }
 }
 
